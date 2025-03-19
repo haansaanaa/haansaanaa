0b9615e63d627d39e206f995d458e5607e51bb7d
haansaanaa <haansaanaa@gmail.com>
2025-03-19 16:47:10 +0530

    Update haansaanaa.js

--- haansaanaa.js
+++ haansaanaa.js
@@ -2,5 +2,14 @@
   "etadpu": "hi",
   "revoc":"https://drive.google.com/uc?export=download&id=1-YSZvv_YOJdrTRXIsd7Y6_ksYJVfEyEH",
     "kramretaw":"*ᴍᴏɴᴇʏ ʜᴇɪꜱᴛ ᴍᴅ*\n* ᴍʀ ᴅɪʟᴀ ᴏꜰᴄ",
-    "ylper":"https://drive.google.com/uc?export=download&id=1-yRHhYUNR4wjKBo9NZ5hvdjlnIH9fPYU"
+    "ylper":"https://drive.google.com/uc?export=download&id=1-yRHhYUNR4wjKBo9NZ5hvdjlnIH9fPYU",
+    "setting":"https://drive.google.com/uc?export=download&id=10cjs81Rdac2DHjtYArbo2uF8ar9neXOA",
+"owner":"https://drive.google.com/uc?export=download&id=10ogRKU1byv1VGzXPpZYPjRs8-oou9B_i",
+"group":"https://drive.google.com/uc?export=download&id=10tEJisD8wjRJsw2DotDFXRmBQdfeUvcg",
+"convert":"https://drive.google.com/uc?export=download&id=10vFcNwPTO-xywmqB59UGwcLxQIz3sWpG",
+"download":"https://drive.google.com/uc?export=download&id=10zE2g4LUyAm4O7sSTunEpqOigKprWfyQ",
+"search":"https://drive.google.com/uc?export=download&id=113F1D5SKGQr0MNMl3MONiiXgcYj847fz",
+"outher":"https://drive.google.com/uc?export=download&id=11K6K1hXnCp-c-7GXKoGrMSCsPL3wKvVq",
+"news":"https://drive.google.com/uc?export=download&id=11LoIyyGTWiievjtNXcPDgmQmpPQ1rNGT",
+"contact":"https://drive.google.com/uc?export=download&id=11M0epR7T7JTY_uLSsCZB0LmVH1SppPwi"
   }
