05b78e0151dbbb905ad5db3fe24ad1ac36f3572f
haansaanaa <haansaanaa@gmail.com>
2025-03-19 14:49:32 +0530

    Update haansaanaa.js

--- haansaanaa.js
+++ haansaanaa.js
@@ -2,5 +2,5 @@
   "etadpu": "hi",
   "revoc":"https://drive.google.com/uc?export=download&id=1-YSZvv_YOJdrTRXIsd7Y6_ksYJVfEyEH",
     "kramretaw":"*ᴍᴏɴᴇʏ ʜᴇɪꜱᴛ ᴍᴅ*\n* ᴍʀ ᴅɪʟᴀ ᴏꜰᴄ",
-    "ylper":"https://drive.google.com/uc?export=download&id=1-5sCEA4D2IPBaHAPuyxgo-HTV16QUJas"
+    "ylper":"https://drive.google.com/uc?export=download&id=1-yRHhYUNR4wjKBo9NZ5hvdjlnIH9fPYU"
   }
