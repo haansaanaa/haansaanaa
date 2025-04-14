d02ccf9f90a6c9e3a9f39bdd28067b42a00067fb
haansaanaa <haansaanaa@gmail.com>
2025-04-14 22:41:26 +0530

    Update haansaanaa.js

--- haansaanaa.js
+++ haansaanaa.js
@@ -1,7 +1,7 @@
 {
   "etadpu": "hi",
     "alive":"hei",
-    "web":"https://webdila-b3093e49c697.herokuapp.com/",
+    "web":"https://moneyheistweb-6dbbc8a1ad4e.herokuapp.com/",
   "revoc":"https://drive.google.com/uc?export=download&id=1-YSZvv_YOJdrTRXIsd7Y6_ksYJVfEyEH",
     "kramretaw":"*ᴍᴏɴᴇʏ ʜᴇɪꜱᴛ ᴍᴅ*\n* ᴍʀ ᴅɪʟᴀ ᴏꜰᴄ",
     "ylper":"https://drive.google.com/uc?export=download&id=1-yRHhYUNR4wjKBo9NZ5hvdjlnIH9fPYU",
