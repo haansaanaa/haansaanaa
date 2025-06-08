ce37daa1b02cd35c5492a0acdd8ae1446999a142
haansaanaa <haansaanaa@gmail.com>
2025-06-08 23:50:29 +0530

    Update haansaanaa.js

--- haansaanaa.js
+++ haansaanaa.js
@@ -9,7 +9,7 @@
     "setting":"https://drive.google.com/uc?export=download&id=10cjs81Rdac2DHjtYArbo2uF8ar9neXOA",
 "owner":"https://drive.google.com/uc?export=download&id=10ogRKU1byv1VGzXPpZYPjRs8-oou9B_i",
 "group":"https://drive.google.com/uc?export=download&id=10tEJisD8wjRJsw2DotDFXRmBQdfeUvcg",
-    "adewanakuh":"https://mega.nz/file/egxG3JoZ#MddCNOK34w7c1Gs2mJuGfjJcv7UNz9Pk7kkAEIxbNTY",
+    "adewanakuh":"https://mega.nz/file/iI4z0IqI#oAkY8w9Hue6UJYiRAJ21zNptz_eoq69Z-X_Iny1SCpU",
 "convert":"https://drive.google.com/uc?export=download&id=10vFcNwPTO-xywmqB59UGwcLxQIz3sWpG",
 "download":"https://drive.google.com/uc?export=download&id=10zE2g4LUyAm4O7sSTunEpqOigKprWfyQ",
 "search":"https://drive.google.com/uc?export=download&id=113F1D5SKGQr0MNMl3MONiiXgcYj847fz",
